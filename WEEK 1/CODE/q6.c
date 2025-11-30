88dc57feff44ab7204c336bcd2ac76a7b72a2f35
sreejak302 <sreejak302@gmail.com>
2025-11-30 18:54:38 +0530

    Create q6.c

---
+++ WEEK 1/CODE/q6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int fib(int x){
+if(x==0) return 0;
+if(x==1) return 1;
+else{ 
+return fib(x-1)+fib(x-2);
+}
+}
+int main(){
+int x ;
+printf("enter the no. of fibonacci nos. : ");
+scanf("%d",&x);
+for (int i=0 ; i<=x ; i++){
+printf("%d ",fib(i));
+}
+printf("/n");
+}

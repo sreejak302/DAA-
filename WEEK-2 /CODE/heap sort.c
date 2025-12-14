ad9fcb40ef02c0254697d066bcb557228d41813e
sreejak302 <sreejak302@gmail.com>
2025-12-14 13:54:26 +0530

    Create heap sort.c

---
+++ WEEK-2 /CODE/heap sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+void heapify(int arr[], int n, int i) {
+int largest = i;
+int left = 2 * i + 1;
+int right = 2 * i + 2; 
+int temp;
+if (left < n && arr[left] > arr[largest])
+largest = left;
+if (right < n && arr[right] > arr[largest])
+largest = right;
+if (largest != i) {
+temp = arr[i];
+arr[i] = arr[largest];
+arr[largest] = temp;
+heapify(arr, n, largest);
+}
+}
+void heapSort(int arr[], int n) {
+int i, temp;
+for (i = n / 2 - 1; i >= 0; i--)
+heapify(arr, n, i);
+for (i = n - 1; i > 0; i--) {
+temp = arr[0];
+arr[0] = arr[i];
+arr[i] = temp;
+heapify(arr, i, 0);
+}
+}
+int main() {
+int n, i;
+int arr[100];
+printf("Enter number of elements: ");
+scanf("%d", &n);
+printf("Enter %d elements:\n", n);
+for (i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+heapSort(arr, n);
+printf("Sorted array:\n");
+for (i = 0; i < n; i++) {
+printf("%d ", arr[i]);
+}
+printf("\n");
+return 0;
+}

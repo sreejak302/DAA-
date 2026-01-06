903509e7da6d1d5c5c857605a55bfbd86a020f9c
sreejak302 <sreejak302@gmail.com>
2026-01-06 22:28:28 +0530

    Create bst.c

---
+++ WEEK-5/CODE/bst.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+    if (root->data == key)
+        return 1;
+    if (key < root->data)
+        return search(root->left, key);
+    return search(root->right, key);
+}
+int main() {
+    struct Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\nPreorder Traversal: ");
+    preorder(root);
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+    int key = 40;
+    if (search(root, key))
+        printf("\n%d found in BST", key);
+    else
+        printf("\n%d not found in BST", key);
+    return 0;
+}
